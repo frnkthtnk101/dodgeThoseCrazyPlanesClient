bdf14622a3554b8800d6ee9beddd28a5c844143f
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-05-25 13:55:15 -0400

    Create level_reader.py
    level reader class created

---
+++ Source/level_reader.py
@@ -0,0 +1,12 @@
+from json import *
+
+
+
+
+'''
+class 
+with open('/Users/francopettigrosso/Desktop/DodgeTheCrazyPlanes/data.json') as jsonfile:
+    cheese = load(jsonfile)
+    print(cheese['Data']['Diffculty'])
+    jsonfile.close()
+'''
