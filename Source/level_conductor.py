fc055f20e7d0e6df41785d16133e57a43830e76f
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-05-25 13:15:58 -0400

    Update level_conductor.py
    added the list to th leve conductor

--- Source/level_conductor.py
+++ Source/level_conductor.py
@@ -3,4 +3,8 @@ import pygame
 
 class level_conductor:
     def __init__(self):
-        self.
+        self.all_sprites_list = pygame.sprite.Group()
+        self.stars_list= pygame.sprite.Group()
+        self.bad_list=pygame.sprite.Group()
+        self.block_list = pygame.sprite.Group()
+        self.hero = TheGUY()
