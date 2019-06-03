d96e767d2e3b4fbca2acb988f9f14b975ca6a3f5
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-06-03 19:15:17 -0400

    Update client_handler.test.py
    nad level test completed

--- Source/client.tests/client_handler.test.py
+++ Source/client.tests/client_handler.test.py
@@ -23,10 +23,30 @@ class client_handler_should(unittest.TestCase):
                 raise Exception('we couldnt close the game')
 
     def test_should_send_bad_level(self):
-        raise Exception('Not ready')
+        we_got_a_level, data = get_level('easy', ['downers'])
+        if we_got_a_level:
+            is_level_easy = data['Difficulty'] == 'easy'
+            did_we_get_same_planes = data['PlaneTypes'] == ['downers']
+            self.assertTrue(is_level_easy)
+            self.assertTrue(did_we_get_same_planes)
+        else:
+            self.fail('something went wrong')
 
     def test_should_quit_game(self):
-        raise Exception('Not ready')
+        self.method_quit = True
+        the_game_ended = end_game()
+        self.assertTrue(the_game_ended)
 
     def test_should_get_level(self):
-        raise Exception('Not ready')
+        server_ack_ok, data = send_bad_level([2,5], 'easy', ['downers'])
+        if server_ack_ok:
+            is_level_easy = data['Difficulty'] == 'easy'
+            did_we_get_same_planes = data['PlaneTypes'] == ['downers']
+            self.assertTrue(is_level_easy)
+            self.assertTrue(did_we_get_same_planes)
+        else:
+            self.fail('something went wrong')
+
+
+if __name__ == "__main__":
+    unittest.main()
