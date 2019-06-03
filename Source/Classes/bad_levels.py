d1aea3cc971bd7eb18c658530dc6ebc2ca43614d
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-06-02 21:06:26 -0400

    Create bad_levels.py
    bad levels enum

---
+++ Source/Classes/bad_levels.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class bad_levels(Enum):
+    different_version = 1
+    different_planes = 2
+    complete_type_not_valid = 3
+    ticks_go_over = 4
+    different_difficulty = 5
