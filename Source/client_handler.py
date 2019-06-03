ae0e4e405d915c7b0b5f3134fa2e9d6e2889c9b0
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-06-02 20:26:06 -0400

    Update client_handler.py
    quickly describes what everything does

--- Source/client_handler.py
+++ Source/client_handler.py
@@ -1,11 +1,50 @@
-from json import *
+'''
+client_handler.py
+this is the skeleton 
+for the client's conn-
+ection to the server
+'''
 
 class client_hander:
-    def __init__(self, file):
-        self.file = file
+    '''
+    defines the what a session_id
+    is
+    '''
+    def __init__(self):
+        self.session_id = None
     
+    '''
+    used to create a game
+    '''
+    def initialize_game(self):
+        raise Exception('not created')
+    
+    '''
+    used to report a bad level given
+    '''
+    def send_bad_level(self):
+        raise Exception('not created')
+    
+    '''
+    used when the player dies
+    '''
+    def end_game(self):
+        raise Exception('not created')
+    
+    '''
+    used when the player quits
+    '''
+    def quit_game(self):
+        raise_Exception('not created')
+    
+    '''
+    used to get a new level
+    '''
     def get_level(self):
-        with open(self.file) as json_file:
-            temp = load(json_file)
-            json_file.close()
-            return temp
+        raise Exception('not created')
+
+    '''
+    used to send information to the machine
+    '''
+    def send():
+        raise Exception('not created')
